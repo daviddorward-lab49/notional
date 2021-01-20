140814518f9bdd401a9182e6ef7ba286fcc2d538
David Dorward <david.dorward@lab49.com>
2021-01-20 14:28:24 +0000

    Test content editable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ContentEditable from "react-contenteditable";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class MyNumber extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.contentEditable = React.createRef();
+    this.state = { html: props.value };
+  }
+
+  handleChange = (evt) => {
+    this.setState({ html: evt.target.value });
+    console.log("Change: ", evt.target.value, evt);
+  };
+
+  handleKeyPress = (evt) => {
+    if (evt.key === "Enter") this.handleFinishedInput(evt);
+  };
+
+  handleBlur = (evt) => {
+    console.log("Blur", evt);
+    this.handleFinishedInput(evt);
+  };
+
+  handleFinishedInput = (evt) => {
+    alert(this.state.html);
+  };
+
+  render = () => (
+    <ContentEditable
+      innerRef={this.contentEditable}
+      html={this.state.html} // innerHTML of the editable div
+      disabled={false} // use true to disable editing
+      onChange={this.handleChange} // handle innerHTML change
+      onKeyPress={this.handleKeyPress}
+      onBlur={this.handleBlur}
+      tagName="span" // Use a custom HTML tag (uses a div by default)
+    />
   );
 }
+
+const App = () => {
+  const cur = "GBP";
+  const value = "100000";
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "30rem",
+        padding: "0.5rem",
+        background: "#aaaaaa",
+      }}
+    >
+      <span style={{ marginRight: "1rem" }}>😀</span>
+      <span>
+        <MyNumber value={value} />
+        <span style={{ marginLeft: "0.5rem" }}>{cur}</span>
+      </span>
+      <span style={{ marginLeft: "1rem" }}>👍</span>
+    </div>
+  );
+};
 
 export default App;
